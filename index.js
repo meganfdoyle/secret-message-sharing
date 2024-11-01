606222e69ca0c5c333a63056148bb3adb5305653
megan <meganfdoyle@gmail.com>
2024-11-01 18:22:01 -0400

    Add card and encryption logic

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-console.log('Hello world!');
+const {hash} = window.location;
+
+ const message = atob(hash.replace('#', ''));
+
+ if (message) {
+    document.querySelector('#message-form').classList.add('hide');
+    document.querySelector('#message-show').classList.remove('hide');
+
+    document.querySelector('h1').innerHTML = message;
+ };
+
+document.querySelector('form').addEventListener('submit', event => {
+    //no backend server to send this to
+    event.preventDefault();
+
+    document.querySelector('#message-form').classList.add('hide');
+    document.querySelector('#link-form').classList.remove('hide');
+
+    const input = document.querySelector('#message-input');
+    const encrypted = btoa(input.value);
+
+    const linkInput = document.querySelector('#link-input')
+    linkInput.value = `${window.location}#${encrypted}`;
+    //selects the input
+    linkInput.select()
+});
+
+/*
+Parts of a URL
+ - Domain
+   - 
+ - Path
+   - 
+ - Query String
+   - Used to communicate options to a backend server
+ - Hash/fragment
+   - Used by the browser exclusively
+
+ message.com  -  /index.html/  -  ?color=red  -  #value
+
+ in the console, window.location is an object that contains 
+  info that describes the current url
+*/
+
